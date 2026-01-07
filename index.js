32ab262604f11809777b8f7cb861a545e2aa8aba
Parth <parthwani22@gmail.com>
2026-01-07 06:41:16 +0530

    Inital setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config({
+    path: './.env'
+});
+
+// Debug Log
+// console.log(process.env);
+
+
+
