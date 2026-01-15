34691b41752cb2fa48d8a858ecd6d5906cb882db
Parth <parthwani22@gmail.com>
2026-01-15 19:36:25 +0530

    Update redis model from hash to key-value model.

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/:link', async(req, res) => {
         if(!input || input.length < process.env.LINK_LENGTH) {
             return res.status(400).send({message: "Invalid input"});
         };
-        const long_url = await client.hGet(`links:${req.params.link}`, 'l_url');
+        const long_url = await client.get(`links:${req.params.link}`);
         // Debug log
         // console.log(long_url);
         if(long_url) {
@@ -68,14 +68,10 @@ app.post('/api/link', async (req,res) => {
     };
     const new_url_nano = nanoid(process.env.LINK_LENGTH);
     const new_url = `http://127.0.0.1:${process.env.PORT}/${new_url_nano}`;
-    const url_map = {
-        s_url: new_url,
-        l_url: url
-    };
     // Debug log
     // console.log(url_map);
     try {
-        await client.hSet(`links:${new_url_nano}`, url_map);
+        await client.set(`links:${new_url_nano}`, url);
     } catch (error) {
         console.error(error);
         return res.status(502).send({message: "Bad Gateway: Unable to process request"})
