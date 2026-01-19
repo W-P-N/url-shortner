b2b785efa6708ab8035f220e980e94a6a0253ea9
Parth <parthwani22@gmail.com>
2026-01-19 18:41:13 +0530

    docker config setup

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,13 +2,7 @@ import pino from "pino";
 import AppConfig from "./env.js";
 
 const logger = pino({
-    level: AppConfig.NODE_ENV === 'production' ? 'info' : 'debug',
-    transport: AppConfig.NODE_ENV !== 'production' ? {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
-        }
-    } : undefined
+    level: AppConfig.NODE_ENV === 'production' ? 'info' : 'debug'
 });
 
 export default logger;
